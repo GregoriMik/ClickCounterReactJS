302627d17a256d0e4aef693c5c997ec023aa601d
Gregori Mikina <gregorimikina@gmail.com>
2021-03-16 23:21:40 +0100

    Component for buttons

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ class Counter extends React.Component{
     result: 0
   }
 //odejmowanie
-  handleMathClick(type, number){
-    if(type === "substraction"){
+  handleMathClick=(type, number)=>{
+    if(type === "subtraction"){
       this.setState(prevState => ({
         count: prevState.count +1,
         result: prevState.result - number
       }))
     }
+//reset
     else if(type==="reset")
       this.setState(prevState => ({
         result: 0
@@ -18,24 +19,57 @@ class Counter extends React.Component{
     else if(type==="addition")
       this.setState(prevState => ({
         count: prevState.count +1,
-        result: prevState.result - number
+        result: prevState.result + number
       }))
   }
   render(){
     return(
       <>
-      <button onClick={this.handleMathClick.bind(this,"substraction",10)}>-10</button>
-      <button onClick={this.handleMathClick.bind(this,"substraction",1)}>-1</button>
-      <button onClick={this.handleMathClick.bind(this,"reset")}>Reset</button>
-      <button onClick={this.handleMathClick.bind(this,"addition",-1)}>1</button>
-      <button onClick={this.handleMathClick.bind(this,"addition",-10)}>10</button>
-
-
+      <MathButton 
+      name="-10" 
+      number="10" 
+      type="subtraction" 
+      click={this.handleMathClick}
+      />
+      <MathButton 
+      name="-1" 
+      number="1" 
+      type="subtraction" 
+      click={this.handleMathClick}
+      />
+      <MathButton 
+      name="reset" 
+      type="reset" 
+      click={this.handleMathClick}
+      />
+      <MathButton 
+      name="+1" 
+      number="1" 
+      type="addition" 
+      click={this.handleMathClick}
+      />
+      <MathButton 
+      name="+10" 
+      number="10" 
+      type="addition" 
+      click={this.handleMathClick}
+      />
       <h1>Liczba kliknięć: {this.state.count}</h1>
       <h1>Wynik: {this.state.result}</h1>
       </>
     )
   }
 }
+
+
+
+const MathButton = (props) =>{
+  const number = parseInt(props.number)// str to int <=====
+  return(
+  <button onClick={() => props.click(props.type, number)}>{props.name}</button>
+  )
+}
+
+
 
 ReactDOM.render(<Counter/>, document.getElementById('root'));
